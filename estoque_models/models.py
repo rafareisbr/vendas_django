d79e20708f0217671d18f98132f8ae3c381be6d7
Rafael Reis <rafareisbr@gmail.com>
2020-09-01 00:11:55 -0300

    add fabricante e preco no produto

--- estoque_models/models.py
+++ estoque_models/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255, unique=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    fabricante = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nome
